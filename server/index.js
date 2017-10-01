0a13285b1f8fe572779461611ea5a418343d646e
ccheung0926 <cindycheung0926@gmail.com>
2017-10-01 00:51:37 -0700

    added twitter api and working D3 now

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 var path = require('path');
+var request = require('request');
 var express = require('express');
 
 var app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
-app.use('/csv', express.static(__dirname + '/public'));
-
+// app.use('/csv', express.static(__dirname + '/public'));
 
 app.listen(3000, function() {
   console.log('listening');
